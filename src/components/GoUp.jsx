b42516005e4da17d9d3ee619e6217036dc1a5d1e
Biruk Worku <bworku1221@gmail.com>
2024-10-27 21:13:42 +0300

    update goup button

--- src/components/GoUp.jsx
+++ src/components/GoUp.jsx
@@ -2,12 +2,12 @@ function GoUp() {
   return (
     <a
       href="#home"
-      className="fixed bottom-8 right-8 lg:bottom-36 lg:right-12 bg-gray-900  rounded-full border-[1px] border-color-primary-l z-auto "
+      className="fixed bottom-24 right-10  lg:bottom-24 lg:right-7 bg-gray-900  rounded-full border-[1px] border-color-primary-l z-auto "
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="#11d8c7"
-        className="h-9 w-9  lg:w-14 lg:h-14"
+        className="h-9 w-9  lg:w-12 lg:h-12"
         viewBox="0 0 256 256"
       >
         <path d="M213.66,165.66a8,8,0,0,1-11.32,0L128,91.31,53.66,165.66a8,8,0,0,1-11.32-11.32l80-80a8,8,0,0,1,11.32,0l80,80A8,8,0,0,1,213.66,165.66Z"></path>
